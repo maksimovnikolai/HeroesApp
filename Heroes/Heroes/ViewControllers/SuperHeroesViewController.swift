69243e419b4219bffbbee7968a2b99f8d3edad44
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-07 18:38:29 +0300

    создан контроллер представлени - SuperHeroesViewController

---
+++ Heroes/Heroes/ViewControllers/SuperHeroesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SuperHeroesViewController.swift
+//  Heroes
+//
+//  Created by Nikolai Maksimov on 07.01.2024.
+//
+
+import UIKit
+
+final class SuperHeroesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
