ccfbc656e98abb3b7c744cf63e7958b43476ad0d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-07 18:31:20 +0300

    создан networkmanager + метод загрузки и декодирования json

---
+++ Heroes/Heroes/Services/NetworkManager.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkManager.swift
+//  Heroes
+//
+//  Created by Nikolai Maksimov on 07.01.2024.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(completion: @escaping(Result<[Superhero], NetworkError>) -> Void) {
+        guard let url = URL(string: Api.heroesUrl.rawValue) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "no error description")
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let superheroes = try JSONDecoder().decode([Superhero].self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(superheroes))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+}
