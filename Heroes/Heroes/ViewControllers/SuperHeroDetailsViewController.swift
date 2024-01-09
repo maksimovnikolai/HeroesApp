2a25bcdfe25db6e6c55030c1f4770a3c0bab848b
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-09 17:51:22 +0300

    + пустой экран детальной информации персонажа

---
+++ Heroes/Heroes/ViewControllers/SuperHeroDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroDetailsViewController.swift
+//  Heroes
+//
+//  Created by Nikolai Maksimov on 09.01.2024.
+//
+
+import UIKit
+
+final class SuperHeroDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    
+    }
+}
